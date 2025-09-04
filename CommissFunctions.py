eecee1c8984aca62ff7f8ae6ed1a22bda9d2bb63
benhord <benjhord@gmail.com>
2025-09-04 16:55:35 -0400

    Added catches for FFIs and ROI selection

--- CommissFunctions.py
+++ CommissFunctions.py
@@ -30,8 +30,49 @@ NIR_dec_shape = (p.NIRDA.shape[1]*u.pix) * p.NIRDA.pixel_scale #arsecs
 # -
 
 def generate_task_plan(variables, output_file):
-    
-    template_str = '''<?xml version="1.0" ?>
+    if variables['ffi_flag']:
+        vis_mode_keys = '''
+                <AcquireVisCamImages>
+                    <TargetID>{VIS_targetID}</TargetID>
+                    <ROI_StartX>512</ROI_StartX>
+                    <ROI_StartY>512</ROI_StartY>
+                    <ROI_SizeX>1024</ROI_SizeX>
+                    <ROI_SizeY>1024</ROI_SizeY>
+                    <NumExposures>{VIS_NumTotalFramesRequested}</NumExposures>
+                    <ExposureTime_us>{VIS_ExposureTime_us}</ExposureTime_us>
+                </AcquireVisCamImages>
+            '''
+    else:
+        if not variables['VIS_StarRoiDetMethod']:
+            roi_keys = ('''
+                    <numPredefinedStarRois>{VIS_numPredefinedStarRois}</numPredefinedStarRois>
+                    <PredefinedStarRoiRa>{VIS_PredefinedStarRoiRa}</PredefinedStarRoiRa>
+                    <PredefinedStarRoiDec>{VIS_PredefinedStarRoiDec}</PredefinedStarRoiDec>
+                    ''')
+        else:
+            roi_keys = '''
+   '''
+        vis_mode_keys = ('''
+                <AcquireVisCamScienceData>
+                    <NumExposuresMax>{VIS_NumExposuresMax}</NumExposuresMax>
+                    <IncludeFieldSolnsInResp>{VIS_IncludeFieldSolnsInResp}</IncludeFieldSolnsInResp>
+                    <ROI_StartX>512</ROI_StartX>
+                    <ROI_StartY>512</ROI_StartY>
+                    <ROI_SizeX>1024</ROI_SizeX>
+                    <ROI_SizeY>1024</ROI_SizeY>
+                    <TargetID>{VIS_targetID}</TargetID>
+                    <TargetRA>{VIS_TargetRA}</TargetRA>
+                    <TargetDEC>{VIS_TargetDEC}</TargetDEC>
+                    <StarRoiDetMethod>{VIS_StarRoiDetMethod}</StarRoiDetMethod>''' + roi_keys +
+'''                 <FramesPerCoadd>{VIS_FramesPerCoadd}</FramesPerCoadd>
+                    <ExposureTime_us>{VIS_ExposureTime_us}</ExposureTime_us>
+                    <MaxNumStarRois>{VIS_MaxNumStarRois}</MaxNumStarRois>
+                    <StarRoiDimension>{VIS_StarRoiDimension[0]}</StarRoiDimension>
+                    <NumTotalFramesRequested>{VIS_NumTotalFramesRequested}</NumTotalFramesRequested>
+                </AcquireVisCamScienceData>
+            ''')
+
+    template_str = ('''<?xml version="1.0" ?>
 <ScienceCalendar xmlns="/pandora/calendar/">
 <Meta Valid_From="2025-04-25 00:00:00" Expires="2026-04-24 12:29:00" Calendar_Weights="0.0, 0.0, 1.0" Ephemeris="sma=6828.14, ecc=0.0, inc=97.2188, aop=0.0, raan=303.263, ta=0.0" Keepout_Angles="90.0, 40.0, 63.0" Created="2024-03-15 14:20:52.506870" Delivery_Id=""/>
     <Visit>
@@ -67,35 +108,14 @@ def generate_task_plan(variables, output_file):
                     <SC_ReadFrames>{NIR_SC_ReadFrames}</SC_ReadFrames>
                     <SC_Groups>{NIR_SC_Groups}</SC_Groups>
                     <SC_Integrations>{NIR_SC_Integrations}</SC_Integrations>
-                </AcquireInfCamImages>
-                <AcquireVisCamScienceData>
-                    <NumExposuresMax>{VIS_NumExposuresMax}</NumExposuresMax>
-                    <IncludeFieldSolnsInResp>{VIS_IncludeFieldSolnsInResp}</IncludeFieldSolnsInResp>
-                    <ROI_StartX>512</ROI_StartX>
-                    <ROI_StartY>512</ROI_StartY>
-                    <ROI_SizeX>1024</ROI_SizeX>
-                    <ROI_SizeY>1024</ROI_SizeY>
-                    <TargetID>{VIS_targetID}</TargetID>
-                    <TargetRA>{VIS_TargetRA}</TargetRA>
-                    <TargetDEC>{VIS_TargetDEC}</TargetDEC>
-                    <StarRoiDetMethod>{VIS_StarRoiDetMethod}</StarRoiDetMethod>
-                    <numPredefinedStarRois>{VIS_numPredefinedStarRois}</numPredefinedStarRois>
-                    <PredefinedStarRoiRa>{VIS_PredefinedStarRoiRa}</PredefinedStarRoiRa>
-                    <PredefinedStarRoiDec>{VIS_PredefinedStarRoiDec}</PredefinedStarRoiDec>
-                    <FramesPerCoadd>{VIS_FramesPerCoadd}</FramesPerCoadd>
-                    <ExposureTime_us>{VIS_ExposureTime_us}</ExposureTime_us>
-                    <MaxNumStarRois>{VIS_MaxNumStarRois}</MaxNumStarRois>
-                    <StarRoiDimension>{VIS_StarRoiDimension}</StarRoiDimension>
-                    <NumTotalFramesRequested>{VIS_NumTotalFramesRequested}</NumTotalFramesRequested>
-                </AcquireVisCamScienceData>
-            </Payload_Parameters>
-        </Observation_Sequence>'''
-    
+                </AcquireInfCamImages>''' + vis_mode_keys +
+         '''</Payload_Parameters>
+        </Observation_Sequence>''')
+
     # Substitute variables in the template string
     formatted_str = template_str.format(**variables)
 
     # Save the formatted string to a file
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(formatted_str)
-
 
